d61f365700d5d4d28b9c27330a27e81f4cc29bc7
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-07-24 20:48:41 -0400

    Adds login

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,8 +36,13 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next',
+    'nuxt-vuex-localstorage'
   ],
+  auth: {
+    // Options
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
